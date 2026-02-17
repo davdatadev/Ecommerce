b515269ca82c653a29fcdb4d40f9d3c552defc30
David Arenas <datadavdev@gmail.com>
2026-02-16 20:41:25 -0500

    Add: configurar dotenv para manejar variables de entorno y actualizar constantes en el proyecto

--- src/app.js
+++ src/app.js
@@ -10,13 +10,14 @@ import productRouter from './routes/productRouter.js';
 import cartRouter from './routes/cartRouter.js';
 import viewsRouter from './routes/viewsRouter.js';
 import sessionsRouter from './routes/sessionsRouter.js';
-import __dirname from './utils/constantsUtil.js';
+import __dirname, { constants } from './utils/constantsUtil.js';
 import websocket from './websocket.js';
 
 const app = express();
 
-const uri = "mongodb+srv://coderhouse_teacher:XS7ToHN00UWBnACA@cluster0.4dcctnp.mongodb.net/demo-db";
-mongoose.connect(uri)
+const MONGO_URL = constants.MONGO_URL
+
+mongoose.connect(MONGO_URL)
     .then(() => console.log("Conectado a la base de datos"))
     .catch(error => console.error("Error en la conexión:", error))
 
@@ -41,7 +42,7 @@ app.use('/api/carts', cartRouter);
 app.use('/api/sessions', sessionsRouter);
 app.use('/', viewsRouter);
 
-const PORT = 8080;
+const PORT = constants.PORT;
 const httpServer = app.listen(PORT, () => {
     console.log(`Start server in PORT ${PORT}`);
 });
--- src/utils/constantsUtil.js
+++ src/utils/constantsUtil.js
@@ -1,11 +1,18 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export const constants = {
     JWT_COOKIE_NAME: 'cookieToken', 
-    JWT_SECRET: 'CoderToken123'
+    JWT_SECRET: 'CoderToken123',
+    PORT: process.env.PORT || 8080,
+    MONGO_URL: process.env.MONGO_URL,
+    JWT_SECRET: process.env.JWT_SECRET || 'SecretKeyDefault',
+    JWT_COOKIE_NAME: process.env.JWT_COOKIE_NAME || 'cookieToken'
 }
 
 export default __dirname;
